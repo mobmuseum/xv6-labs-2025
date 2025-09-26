392e7f421817177cd0f7468eb26276a25dc30759
Aadesh <aadesh.kumparp2004@gmail.com>
2025-09-26 18:12:38 +0500

    Modified find.c to support regex

--- user/find.c
+++ user/find.c
@@ -2,12 +2,62 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 #include "kernel/fs.h"
-#include "kernel/param.h" 
+#include "kernel/param.h"
 
 char buf[512];
 
+// ---------------- Regex matcher (from grep.c) ----------------
+int matchhere(char*, char*);
+int matchstar(int, char*, char*);
+
+int
+match(char *re, char *text)
+{
+  if(re[0] == '^')
+    return matchhere(re+1, text);
+  do{  // must look at empty string
+    if(matchhere(re, text))
+      return 1;
+  }while(*text++ != '\0');
+  return 0;
+}
+
+int
+matchhere(char *re, char *text)
+{
+  if(re[0] == '\0')
+    return 1;
+  if(re[1] == '*')
+    return matchstar(re[0], re+2, text);
+  if(re[0] == '$' && re[1] == '\0')
+    return *text == '\0';
+  if(*text!='\0' && (re[0]=='.' || re[0]==*text))
+    return matchhere(re+1, text+1);
+  return 0;
+}
+
+int
+matchstar(int c, char *re, char *text)
+{
+  do{  // a * matches zero or more instances
+    if(matchhere(re, text))
+      return 1;
+  }while(*text!='\0' && (*text++==c || c=='.'));
+  return 0;
+}
+// -------------------------------------------------------------
+
+// helper: return last path component (filename)
+char*
+basename(char *path) {
+  char *p = path + strlen(path);
+  while(p >= path && *p != '/')
+    p--;
+  return p+1;
+}
+
 void
-find(char *path, char *filename, int doexec, char *cmd[], int cmdargc)
+find(char *path, char *pattern, int doexec, char *cmd[], int cmdargc)
 {
   char buf[512], *p;
   int fd;
@@ -27,7 +77,7 @@ find(char *path, char *filename, int doexec, char *cmd[], int cmdargc)
 
   switch(st.type){
   case T_FILE:
-    if(strcmp(path+strlen(path)-strlen(filename), filename) == 0){
+    if(match(pattern, basename(path))){
       if(doexec){
         // build argv
         char *argv[MAXARG];
@@ -37,8 +87,15 @@ find(char *path, char *filename, int doexec, char *cmd[], int cmdargc)
         argv[cmdargc+1] = 0;
 
         if(fork() == 0){
-          exec(argv[0], argv);
-          fprintf(2, "exec %s failed\n", argv[0]);
+          char prog[32];
+          if(cmd[0][0] != '/'){
+            prog[0] = '/';
+            strcpy(prog+1, cmd[0]);  // xv6 has strcpy
+            exec(prog, argv);
+          } else {
+            exec(cmd[0], argv);
+          }
+          fprintf(2, "exec %s failed\n", cmd[0]);
           exit(1);
         }
         wait(0);
@@ -63,7 +120,7 @@ find(char *path, char *filename, int doexec, char *cmd[], int cmdargc)
         continue;
       memmove(p, de.name, DIRSIZ);
       p[DIRSIZ] = 0;
-      find(buf, filename, doexec, cmd, cmdargc);
+      find(buf, pattern, doexec, cmd, cmdargc);
     }
     break;
   }
@@ -74,7 +131,7 @@ int
 main(int argc, char *argv[])
 {
   if(argc < 3){
-    fprintf(2, "Usage: find <path> <filename> [-exec command ...]\n");
+    fprintf(2, "Usage: find <path> <pattern> [-exec command ...]\n");
     exit(1);
   }
 
